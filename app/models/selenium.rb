b132dbbe4d7949465f0243fbf56b96a5018ab3ef
Doug Grainger <doug.grainger@simplybusiness.com>
2022-04-11 07:43:13 -0400

    simple script that uses selenium to open a web page find and email navigate to another page and assert that the emails are the same

--- app/models/selenium.rb
+++ app/models/selenium.rb
@@ -3,18 +3,27 @@ require 'webdrivers'
 require 'pry'
 
 driver = Selenium::WebDriver.for :chrome
+driver.manage.timeouts.implicit_wait = 3000
 
 driver.get "https://snap.modernemortgage.com/home/mobile-test"
 
-email = driver.find_element(:partial_link_text, '@').text
-binding.pry
-unless email.nil?
+email = driver.find_element(:partial_link_text, '@')
+
+unless email.text.nil?
 	email_text = email.text
 	puts "Success email found: #{email_text}"
 else
 	puts "Failed no email found"
 end
 
+# driver.find_element(:partial_link_text, 'Apply Now').click
 driver.get "https://admin1.himaxwelldemo.com/loan_applications/new?share_token=2530-ce48674a-d0a2-49eb-ae16-e37c08bcf977"
 
-binding.pry
+email2 = driver.find_element(:partial_link_text, '@').text
+
+if email_text == email2
+	puts "Success emails match" 
+else
+	puts "Unsuccessful"
+end
+
